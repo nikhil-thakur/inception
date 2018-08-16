ceca1f9a1b5534fadf6c4b3211528803c86f2038
Nik Thakur <nik.thakur@daugherty.com>
2018-08-15 20:46:44 -0500

    Initial commit

---
+++ src/test/java/repositories/PracticeRepositoryTest.java
@@ -0,0 +1,14 @@
+package repositories;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PracticeRepositoryTest {
+
+    @Test
+    public void shouldAddAndDeletePractice() throws Exception {
+
+    }
+
+}
