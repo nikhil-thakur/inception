bb1ceea36d5e1958eede782a0c030979910e0bfe
Nik Thakur <nik.thakur@daugherty.com>
2018-08-15 20:56:47 -0500

    Adding Practice repository test

--- src/test/java/repositories/PracticeRepositoryTest.java
+++ src/test/java/repositories/PracticeRepositoryTest.java
@@ -1,14 +1,46 @@
 package repositories;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PracticeRepositoryTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PracticeRepository practiceRepository;
 
     @Test
     public void shouldAddAndDeletePractice() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/people").content(
+                "{\"name\": \"Java\"}")).andExpect(
+                status().isCreated()).andReturn();
 
+        String location = mvcResult.getResponse().getHeader("Location");
+        mockMvc.perform(delete(location)).andExpect(status().isNoContent());
+
+        mockMvc.perform(get(location)).andExpect(status().isNotFound());
     }
 
 }
