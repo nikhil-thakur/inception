1e53f2994882ea3fbac3d230e9bfe28a1ba2c582
Nik Thakur <nik.thakur@daugherty.com>
2018-08-18 13:31:52 -0500

    Added Status

---
+++ src/main/java/interview/domain/Status.java
@@ -0,0 +1,23 @@
+package interview.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    boolean complete;
+}
